0c467575a90cb3a9bf885b964328c10e4857d672
erikikoo <erikikoo@hotmail.com>
2018-06-28 22:43:17 -0300

    'updateErrorLogin'

--- app/controllers/api/v1/monitor_users_controller.rb
+++ app/controllers/api/v1/monitor_users_controller.rb
@@ -32,8 +32,8 @@ module Api::V1
       #password_digest = monitor_user_params.password_digest
       @monitor_user.password = monitor_user_params[:password_digest]
       @monitor_user.password_confirmation = monitor_user_params[:password_digest]
-      
-      if @monitor_user.update(monitor_user_params)#@monitor_user.save!
+      #@monitor_user.update(monitor_user_params)#
+      if @monitor_user.save!
         #num = @monitor_user.access_count.to_i + 1
         #@monitor_user.update(access_count: num)
         render json: @monitor_user
